5c14b7ae958cd6a1f939436cdd555113e4dc9469
dackJaniel <daniel.hilmer@outlook.de>
2024-11-07 21:03:23 +0100

    Implement chat functionality with OpenAI API and update frontend for user interaction

--- ChatBot/app/src/js/main.js
+++ ChatBot/app/src/js/main.js
@@ -2,19 +2,54 @@ import "../css/style.css";
 
 import axios from "axios";
 
-async function getTest() {
+const form = document.querySelector("#chat-form");
+const messages = document.querySelector("#messages");
+
+function submitAction(e) {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  const input = data.get("input");
+
+  renderMessage(input, "Benutzer");
+  getAnswer(input);
+}
+
+function renderMessage(message, role = "Benutzer") {
+  if (!message) {
+    console.error("Message is empty.");
+    return;
+  }
+
+  messages.innerHTML += `
+      <div class="w-2/3 bg-white p-5 rounded-md  ${
+        role === "Benutzer" ? "self-end" : "self-start"
+      }">
+        <span class="text-gray-500 text-sm">${role}</span>
+        <p>${message}</p>
+      </div>`;
+}
+
+async function getAnswer(message) {
+  if (!message) {
+    console.error("Message is empty.");
+    return;
+  }
+
   try {
-    const res = await axios.get("http://localhost:3000/");
+    const res = await axios.post("http://localhost:3000/api/chat", {
+      message,
+    });
 
     if (!res) {
       console.log(res);
       throw new Error("Data not found.");
     }
 
-    console.log(res.data.message);
+    renderMessage(res.data.message.content, "ChatGPT");
   } catch (error) {
     console.error(error);
   }
 }
 
-getTest();
+form.addEventListener("submit", (e) => submitAction(e));
--- ChatBot/server/src/index.js
+++ ChatBot/server/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
+const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
 
 if (!PORT) {
   console.error("PORT is not defined");
@@ -17,7 +19,15 @@ if (!HOST) {
   process.exit(1);
 }
 
+if (!OPEN_AI_KEY) {
+  console.error("OPEN_AI_KEY is not defined");
+  process.exit(1);
+}
+
 const server = express();
+const openai = new OpenAI({
+  apiKey: OPEN_AI_KEY,
+});
 
 server.use(express.json());
 server.use(
@@ -27,9 +37,32 @@ server.use(
   })
 );
 
-server.get("/", (req, res) => {
-  res.json({ message: "Hello, World!" });
+server.post("/api/chat", async (req, res) => {
+  if (!req.body.message) {
+    res.status(400).json({ message: "Message is required" });
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Du bist ein hilfreicher Assistent. Gib maximal 2 Sätze mit ingesammt 20 Wörtern zurück.",
+      }, // Prompt Engineering
+      {
+        role: "user",
+        content: req.body.message,
+      },
+    ],
+  });
+  if (completion.choices[0].message.refusal !== null) {
+    res.status(400).json({ message: "Fehler ist aufgetreten." });
+  }
+  res.status(200).json({ message: completion.choices[0].message });
 });
+
+async function name(params) {}
 
 server.listen(PORT, HOST, () => {
   console.log(`Server is running on port ${PORT} and host ${HOST}`);
